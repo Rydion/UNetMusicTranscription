9cc455be0b4ca1c44b9b69b4cbdf151767bd22c7
Adrian Hintze <hintze.adrian@gmail.com>
2018-10-23 20:23:09 +0100

    Changed code to work on city servers.

--- utils/Spectrogram.py
+++ utils/Spectrogram.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from abc import ABC, abstractmethod
-from utils.functions import normalize_array
+from functions import normalize_array
 
 class Spectrogram(ABC):
     @staticmethod
